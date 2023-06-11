f03233da192af1f79dafb3254d95d1b6bd00edd1
AnabellaSimonpietri <simonpietrianabella@gmail.com>
2023-06-11 19:12:36 -0300

    Base del Detail

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,13 +1,44 @@
-import axios from "axios";
+import axios from 'axios';
 
 export function postArts(payload) {
   return async function (dispatch) {
     try {
       const response = await axios.post(
-        "http://localhost:5173/create",
+        'http://localhost:5173/create',
         payload
       );
-      dispatch({ type: "POST_ARTS_SUCCESS", payload: response.data });
+      dispatch({ type: 'POST_ARTS_SUCCESS', payload: response.data });
+      return response;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+}
+
+// Acción para agregar un favorito
+export function addFavorite(payload) {
+  return async function (dispatch) {
+    try {
+      const response = await axios.post(
+        'http://localhost:5173/favorites/add',
+        payload
+      );
+      dispatch({ type: 'ADD_FAVORITE_SUCCESS', payload: response.data });
+      return response;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+}
+
+// Acción para eliminar un favorito
+export function deleteFavorite(payload) {
+  return async function (dispatch) {
+    try {
+      const response = await axios.delete(
+        `http://localhost:5173/favorites/delete/${payload.id}`
+      );
+      dispatch({ type: 'DELETE_FAVORITE_SUCCESS', payload: response.data });
       return response;
     } catch (error) {
       console.error(error);
