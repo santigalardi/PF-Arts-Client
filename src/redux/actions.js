f413fe8aa3267b50a1f54ff0684ee55fe85c4c68
alexrobertCQ <axelromeo63@gmail.com>
2023-06-12 18:01:34 -0500

    gvgh

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,12 +1,10 @@
-import axios from "axios";
+import axios from 'axios';
+
 export function postArts(payload) {
   return async function (dispatch) {
     try {
-      const response = await axios.post(
-        "http://localhost:5173/create",
-        payload
-      );
-      dispatch({ type: "POST_ARTS_SUCCESS", payload: response.data });
+      const response = await axios.post('http://localhost:3001/create', payload);
+      dispatch({ type: 'POST_ARTS_SUCCESS', payload: response.data });
       return response;
     } catch (error) {
       console.error(error);
--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -1,10 +1,10 @@
 import { Container, Row, Col, Form, Button, Image } from 'react-bootstrap';
 import googleLogo from '../../assets/img/google.png';
-// import styles from './Login.modules.css';
+// import styles from './Login.module.css';
 
 const Login = () => {
   return (
-    // className={styles.landing}
+    // className={styles['login-container']}
     <div>
       <Container className='w-100 bg-primary mt-5 rounded shadow'>
         {/* Background */}
@@ -13,31 +13,28 @@ const Login = () => {
           {/*  */}
 
           <Col className='bg-white p-5 rounded-end'>
-            <div className='logo-box text-center'>
-              <Image src='./img/logo.jpg' id='logo' width='100' alt='' />
-            </div>
-            <h2 className='fw-bold text-center py-5'>Inicio de Sesión</h2>
+            <div className='logo-box text-center'>{/* <Image src='./img/logo.jpg' id='logo' width='100' alt='' /> */}</div>
+            <h2 className='fw-bold text-center py-5'>Log In</h2>
             <div className='login'>
               <Form id='login'>
                 <Form.Group className='mb-4'>
-                  <Form.Label htmlFor='user'>Usuario</Form.Label>
+                  <Form.Label htmlFor='user'>User</Form.Label>
                   <Form.Control type='text' id='user' />
                 </Form.Group>
                 <Form.Group className='mb-4'>
-                  <Form.Label htmlFor='pass'>Contraseña</Form.Label>
+                  <Form.Label htmlFor='pass'>Password</Form.Label>
                   <Form.Control type='password' id='pass' />
                 </Form.Group>
                 <div className='d-grid'>
                   <Button variant='primary' type='submit' id='ingresar'>
-                    Ingresar
+                    Log in
                   </Button>
                 </div>
                 <div className='mb-4'>
                   <p className='text-danger mt-2' id='mensaje'></p>
                   {/* ERRORS */}
-                  <span className='mensaje'>¿No tienes cuenta?</span>{' '}
                   <a href='./html/register.html' className='link'>
-                    Regístrate
+                    Register
                   </a>
                 </div>
               </Form>
@@ -45,18 +42,15 @@ const Login = () => {
               {/* LOGIN REDES SOCIALES */}
 
               <Container className='w-100 my-5'>
-                <Row className='text-center'>
-                  <Col>Iniciar sesión con</Col>
-                </Row>
                 <Row>
                   <Col>
                     <Button variant='outline-danger' className='w-100 my-1'>
                       <Row className='align-items-center'>
-                        <Col xs={2} className='d-none d-md-block'>
+                        <Col xs={1} className='d-block'>
                           <Image src={googleLogo} width='34' alt='' />
                         </Col>
-                        <Col xs={12} md={10} className='text-center'>
-                          Google
+                        <Col xs={10} md={10} className='text-center'>
+                          Log in with Google
                         </Col>
                       </Row>
                     </Button>
