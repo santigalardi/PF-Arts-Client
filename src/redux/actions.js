ef6e1953c5faafc6b264a014e5df03d7c67c58a5
AnabellaSimonpietri <simonpietrianabella@gmail.com>
2023-06-12 23:52:36 -0300

    actualizo arreglo de errores en mi repo

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -40,9 +40,6 @@ export function deleteFavorite(payload) {
       );
       dispatch({ type: 'DELETE_FAVORITE_SUCCESS', payload: response.data });
 
-      const response = await axios.post('http://localhost:3001/create', payload);
-      dispatch({ type: 'POST_ARTS_SUCCESS', payload: response.data });
-
       return response;
     } catch (error) {
       console.error(error);
@@ -50,27 +47,12 @@ export function deleteFavorite(payload) {
   };
 }
 
-export const getAllUser=()=>{
-    return async function (dispatch) {
-        const response = await axios ("https://jsonplaceholder.typicode.com/users");
-        return dispatch({
-            type: 'GET_USER',
-            payload: response.data
-        });
-    };
+export const getAllUser = () => {
+  return async function (dispatch) {
+    const response = await axios('https://jsonplaceholder.typicode.com/users');
+    return dispatch({
+      type: 'GET_USER',
+      payload: response.data,
+    });
+  };
 };
-// Acción para agregar un favorito
-export const addFavorite = (name) => {
-    return {
-      type: 'ADD_FAVORITE',
-      payload: name,
-    };
-  };
-  
-  // Acción para eliminar un favorito
-  export const deleteFavorite = (id) => {
-    return {
-      type: 'DELETE_FAVORITE',
-      payload: id,
-    };
-  };
