8ba574051408df61c28536a2ceee3a1d1ff5883b
Santiago Galardi <santygalardi@gmail.com>
2023-06-12 21:21:22 -0300

    arreglando los errors

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -3,10 +3,7 @@ import axios from 'axios';
 export function postArts(payload) {
   return async function (dispatch) {
     try {
-      const response = await axios.post(
-        'http://localhost:5173/create',
-        payload
-      );
+      const response = await axios.post('http://localhost:5173/create', payload);
       dispatch({ type: 'POST_ARTS_SUCCESS', payload: response.data });
       return response;
     } catch (error) {
@@ -19,10 +16,7 @@ export function postArts(payload) {
 export function addFavorite(payload) {
   return async function (dispatch) {
     try {
-      const response = await axios.post(
-        'http://localhost:5173/favorites/add',
-        payload
-      );
+      const response = await axios.post('http://localhost:5173/favorites/add', payload);
       dispatch({ type: 'ADD_FAVORITE_SUCCESS', payload: response.data });
       return response;
     } catch (error) {
@@ -35,13 +29,8 @@ export function addFavorite(payload) {
 export function deleteFavorite(payload) {
   return async function (dispatch) {
     try {
-      const response = await axios.delete(
-        `http://localhost:5173/favorites/delete/${payload.id}`
-      );
+      const response = await axios.delete(`http://localhost:5173/favorites/delete/${payload.id}`);
       dispatch({ type: 'DELETE_FAVORITE_SUCCESS', payload: response.data });
-
-      const response = await axios.post('http://localhost:3001/create', payload);
-      dispatch({ type: 'POST_ARTS_SUCCESS', payload: response.data });
 
       return response;
     } catch (error) {
@@ -50,27 +39,12 @@ export function deleteFavorite(payload) {
   };
 }
 
-export const getAllUser=()=>{
-    return async function (dispatch) {
-        const response = await axios ("https://jsonplaceholder.typicode.com/users");
-        return dispatch({
-            type: 'GET_USER',
-            payload: response.data
-        });
-    };
+export const getAllUser = () => {
+  return async function (dispatch) {
+    const response = await axios('https://jsonplaceholder.typicode.com/users');
+    return dispatch({
+      type: 'GET_USER',
+      payload: response.data,
+    });
+  };
 };
-// Acción para agregar un favorito
-export const addFavorite = (name) => {
-    return {
-      type: 'ADD_FAVORITE',
-      payload: name,
-    };
-  };
-  
-  // Acción para eliminar un favorito
-  export const deleteFavorite = (id) => {
-    return {
-      type: 'DELETE_FAVORITE',
-      payload: id,
-    };
-  };
--- src/views/Form/Form.jsx
+++ src/views/Form/Form.jsx
@@ -1,29 +1,10 @@
-import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import { postArts } from "../../redux/actions";
-import { useDispatch } from "react-redux";
-import styles from "./Form.module.css";
-
-export default function Form() {
-  const dispatch = useDispatch();
-  const [errors, setErrors] = useState({});
-  const [input, setInput] = useState({
-    title: "",
-    image: "",
-    artistName: "",
-    completionYear: "",
-    width: "",
-    height: "",
-    description: "",
-
-import { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom'; //Enlaces e historial de navegacion.
+import { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { postArts } from '../../redux/actions';
-import { useDispatch } from 'react-redux'; //Despacho acciones.
+import { useDispatch } from 'react-redux';
 import styles from './Form.module.css';
 
 function validate(input) {
-  //Validaciones
   let errors = {};
   if (!input.title) {
     errors.title = 'Need a title';
@@ -32,10 +13,10 @@ function validate(input) {
     errors.image = 'Need an image URL';
   }
   if (!input.artistName) {
-    errors.artistName = 'Need a artist name';
+    errors.artistName = 'Need an artist name';
   }
-  if (!input.completitionYear) {
-    errors.completitionYear = 'Need a year';
+  if (!input.completionYear) {
+    errors.completionYear = 'Need a year';
   }
   if (!input.width) {
     errors.width = 'Need a width';
@@ -43,114 +24,57 @@ function validate(input) {
   if (!input.height) {
     errors.height = 'Need a height';
   }
-
   return errors;
 }
 
 export default function Form() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const [errors, setErrors] = useState({}); //almacena errores de validaciones del formu.
+  const [errors, setErrors] = useState({});
   const [input, setInput] = useState({
-    //almacena valores.
     title: '',
     image: '',
     artistName: '',
-    completitionYear: '',
+    completionYear: '',
     width: '',
     height: '',
-
+    description: '',
   });
   const [submitted, setSubmitted] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
   const [showConfirmation, setShowConfirmation] = useState(false);
   const [showScrollButton, setShowScrollButton] = useState(false);
 
-  function validate(input) {
-    let errors = {};
-    if (!input.title) {
-      errors.title = "Need a title";
-    }
-    if (!input.image) {
-      errors.image = "Need an image URL";
-    }
-    if (!input.artistName) {
-      errors.artistName = "Need an artist name";
-    }
-    if (!input.completionYear) {
-      errors.completionYear = "Need a year";
-    }
-    if (!input.width) {
-      errors.width = "Need a width";
-    }
-    if (!input.height) {
-      errors.height = "Need a height";
-    }
-    return errors;
-  }
-
   function handleChange(event) {
-    setInput({
-      ...input,
-
-      [e.target.name]: e.target.value,
-
-      [event.target.name]: event.target.value,
-
-    });
-    setErrors(
-      validate({
-        ...input,
-
-        [e.target.name]: e.target.value,
-
-        [event.target.name]: event.target.value,
-
-      })
-    );
+    const { name, value } = event.target;
+    setInput((prevInput) => ({
+      ...prevInput,
+      [name]: value,
+    }));
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      [name]: '',
+    }));
     setShowAlert(false);
   }
 
   function handleSubmit(e) {
     e.preventDefault();
-    const errors = validate(input);
-    setErrors(errors);
+    const validationErrors = validate(input);
+    setErrors(validationErrors);
     setSubmitted(true);
 
-
-    if (
-      Object.keys(errors).length === 0 &&
-      input.title &&
-      input.image &&
-      input.artistName &&
-      input.completionYear &&
-      input.width &&
-      input.height
-    ) {
-
-    if (Object.keys(errors).length === 0 && input.title && input.image && input.artistName && input.completitionYear && input.width && input.height) {
-
-      const updatedInput = {
-        ...input,
-      };
-      dispatch(postArts(updatedInput));
+    if (Object.keys(validationErrors).length === 0) {
+      dispatch(postArts(input));
       setShowConfirmation(true);
       setInput({
-
-        title: "",
-        image: "",
-        artistName: "",
-        completionYear: "",
-        width: "",
-        height: "",
-
         title: '',
         image: '',
         artistName: '',
-        completitionYear: '',
+        completionYear: '',
         width: '',
         height: '',
-
+        description: '',
       });
       setSubmitted(false);
       setErrors({});
@@ -163,7 +87,7 @@ export default function Form() {
   function handleScrollButton() {
     window.scrollTo({
       top: 0,
-      behavior: "smooth",
+      behavior: 'smooth',
     });
   }
 
@@ -177,176 +101,62 @@ export default function Form() {
       setShowScrollButton(isBottom);
     }
 
-    window.addEventListener("scroll", handleScroll);
+    window.addEventListener('scroll', handleScroll);
     return () => {
-      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener('scroll', handleScroll);
     };
   }, []);
 
   return (
     <div>
-
-      <Link to="/home">
-        <button className={`${styles.button} ${styles.homeButton}`}>
-          Home
-        </button>
+      <Link to='/home'>
+        <button className={`${styles.button} ${styles.homeButton}`}>Home</button>
       </Link>
       <div className={styles.container}>
         <h1 className={styles.heading}>Create a new art!</h1>
         <form onSubmit={handleSubmit}>
-          {showAlert && Object.keys(errors).length > 0 && (
-            <p className={styles.error}>Please fill out all required fields.</p>
-          )}
+          {showAlert && Object.keys(errors).length > 0 && <p className={styles.error}>Please fill out all required fields.</p>}
           <div className={styles.formGroup}>
             <label className={styles.label}>Title: </label>
-            <input
-              type="text"
-              value={input.title}
-              name="title"
-              onChange={handleChange}
-              className={styles.input}
-              placeholder="Enter a title"
-            />
+            <input type='text' value={input.title} name='title' onChange={handleChange} className={styles.input} placeholder='Enter a title' />
             {errors.title && <p className={styles.error}>{errors.title}</p>}
           </div>
           <div className={styles.formGroup}>
             <label className={styles.label}>Image: </label>
-            <input
-              type="text"
-              value={input.image}
-              name="image"
-              onChange={handleChange}
-              className={styles.input}
-              placeholder="Enter an image URL"
-            />
-            {submitted && errors.image && (
-              <p className={styles.error}>{errors.image}</p>
-            )}
+            <input type='text' value={input.image} name='image' onChange={handleChange} className={styles.input} placeholder='Enter an image URL' />
+            {submitted && errors.image && <p className={styles.error}>{errors.image}</p>}
           </div>
           <div className={styles.formGroup}>
             <label className={styles.label}>Artist name: </label>
-            <input
-              type="text"
-              value={input.artistName}
-              name="artistName"
-              onChange={handleChange}
-              className={styles.input}
-              placeholder="Enter an artist name"
-            />
-            {submitted && errors.artistName && (
-              <p className={styles.error}>{errors.artistName}</p>
-            )}
-
-      <Link to='/home'>
-        <button className='button'>Home</button>
-      </Link>
-      <h1>Create a new Art!</h1>
-      <div className={styles['container']}>
-        <form onSubmit={handleSubmit}>
-          {showAlert && Object.keys(errors).length > 0 && <p className='error'>Please fill out all required fields.</p>}
-          <div>
-            <label>Title: </label>
-            <input type='text' value={input.title} name='title' onChange={handleChange} />
-            {errors.title && <p className='error'>{errors.title}</p>}
-          </div>
-          <div>
-            <label>Image: </label>
-            <input type='text' value={input.image} name='image' onChange={handleChange} />
-            {submitted && errors.image && <p className='error'>{errors.image}</p>}
-          </div>
-          <div>
-            <label>Artist name: </label>
-            <input type='text' value={input.artistName} name='artistName' onChange={handleChange} />
-
+            <input type='text' value={input.artistName} name='artistName' onChange={handleChange} className={styles.input} placeholder='Enter an artist name' />
+            {submitted && errors.artistName && <p className={styles.error}>{errors.artistName}</p>}
           </div>
           <div className={styles.formGroup}>
             <label className={styles.label}>Year: </label>
-            <input
-
-              type="text"
-              value={input.completionYear}
-              name="completionYear"
-
-              type='text' // Lo puedo cambiar a "number"
-              value={input.completitionYear}
-              name='completitionYear'
-
-              onChange={handleChange}
-              className={styles.input}
-              placeholder="Enter a year"
-            />
-            {submitted && errors.completionYear && (
-              <p className={styles.error}>{errors.completionYear}</p>
-            )}
+            <input type='text' value={input.completionYear} name='completionYear' onChange={handleChange} className={styles.input} placeholder='Enter a year' />
+            {submitted && errors.completionYear && <p className={styles.error}>{errors.completionYear}</p>}
           </div>
-
           <div className={styles.formGroup}>
             <label className={styles.label}>Width: </label>
-            <input
-              type="number"
-              value={input.width}
-              name="width"
-              onChange={handleChange}
-              className={styles.input}
-              placeholder="Enter a width"
-            />
-            {submitted && errors.width && (
-              <p className={styles.error}>{errors.width}</p>
-            )}
+            <input type='number' value={input.width} name='width' onChange={handleChange} className={styles.input} placeholder='Enter a width' />
+            {submitted && errors.width && <p className={styles.error}>{errors.width}</p>}
           </div>
           <div className={styles.formGroup}>
             <label className={styles.label}>Height: </label>
-            <input
-              type="number"
-              value={input.height}
-              name="height"
-              onChange={handleChange}
-              className={styles.input}
-              placeholder="Enter a height"
-            />
-            {submitted && errors.height && (
-              <p className={styles.error}>{errors.height}</p>
-            )}
+            <input type='number' value={input.height} name='height' onChange={handleChange} className={styles.input} placeholder='Enter a height' />
+            {submitted && errors.height && <p className={styles.error}>{errors.height}</p>}
           </div>
           <div className={styles.formGroup}>
             <label className={styles.label}>Description: </label>
-            <textarea
-              value={input.description}
-              name="description"
-              onChange={handleChange}
-              className={styles.textarea}
-              placeholder="Enter a description..."
-            />
+            <textarea value={input.description} name='description' onChange={handleChange} className={styles.textarea} placeholder='Enter a description...' />
           </div>
-          <button type="submit" className={styles.button}>
+          <button type='submit' className={styles.button}>
             Create
           </button>
-          {showConfirmation && (
-            <p className={styles.confirmation}>Art created successfully!</p>
-          )}
-
-          <div>
-            <label>Width: </label>
-            <input type='text' value={input.width} name='width' onChange={handleChange} />
-          </div>
-          <div>
-            <label>Height: </label>
-            <input type='text' value={input.height} name='height' onChange={handleChange} />
-          </div>
-
-          <button className='button' type='submit'>
-            Create
-          </button>
-          {showConfirmation && <p className='confirmation'>Art created successfully!</p>}
-
+          {showConfirmation && <p className={styles.confirmation}>Art created successfully!</p>}
         </form>
       </div>
-      {showScrollButton && (
-        <button
-          className={styles.scrollButton}
-          onClick={handleScrollButton}
-        ></button>
-      )}
+      {showScrollButton && <button className={styles.scrollButton} onClick={handleScrollButton}></button>}
     </div>
   );
 }
