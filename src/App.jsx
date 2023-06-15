61b5b54ea9154fab2ee9084375d39d1060397966
Santiago Galardi <santygalardi@gmail.com>
2023-06-14 22:46:35 -0300

    Modularizado CardsContainer & CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/NavBar/Navbar';
 import Detail from './views/Detail/Detail';
 import Form from './views/Form/Form';
 import Home from './views/Home/Home';
 import Login from './views/Login/Login';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className='App'>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
         <Route path='/detail/:id' element={<Detail />} />
         <Route path='/create' element={<Form />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
