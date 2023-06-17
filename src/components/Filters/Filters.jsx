8e9ce52766e1ca5786ef33d0c7543de45ac34030
Anabella Simonpietri <simonpietrianabella@gmail.com>
2023-06-17 17:44:01 -0300

    filtros andan solos y combinados

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -23,7 +23,8 @@ const Filters = ({ setCurrentPage }) => {
 
   const handleFilterSubmit = (event) => {
     event.preventDefault();
-    dispatch(getArtsByFilters(filters.century, filters.order, filters.created));
+    const { century, order, created } = filters;
+    dispatch(getArtsByFilters(century, order, created));
     setCurrentPage(1);
   };
 
@@ -31,30 +32,42 @@ const Filters = ({ setCurrentPage }) => {
     <div className={styles.container}>
       <form onSubmit={handleFilterSubmit}>
         <label>
-          <select name='century' value={filters.century} onChange={handleFilterChange}>
-            <option value=''>All</option>
-            <option value='17'>17th Century</option>
-            <option value='18'>18th Century</option>
-            <option value='19'>19th Century</option>
-            <option value='20'>20th Century</option>
-            <option value='21'>21st Century</option>
+          <select
+            name="century"
+            value={filters.century}
+            onChange={handleFilterChange}
+          >
+            <option value="">All</option>
+            <option value="17">17th Century</option>
+            <option value="18">18th Century</option>
+            <option value="19">19th Century</option>
+            <option value="20">20th Century</option>
+            <option value="21">21st Century</option>
           </select>
         </label>
         <label>
-          <select name='order' value={filters.order} onChange={handleFilterChange}>
-            <option value=''>All</option>
-            <option value='ASC'>Ascending</option>
-            <option value='DESC'>Descending</option>
+          <select
+            name="order"
+            value={filters.order}
+            onChange={handleFilterChange}
+          >
+            <option value="">All</option>
+            <option value="ASC">Ascending</option>
+            <option value="DESC">Descending</option>
           </select>
         </label>
         <label>
-          <select name='created' value={filters.created} onChange={handleFilterChange}>
-            <option value=''>All</option>
-            <option value='true'>Created Art</option>
-            <option value='false'>API Art</option>
+          <select
+            name="created"
+            value={filters.created}
+            onChange={handleFilterChange}
+          >
+            <option value="">All</option>
+            <option value="true">Created Art</option>
+            <option value="false">API Art</option>
           </select>
         </label>
-        <button type='submit'>Apply Filters</button>
+        <button type="submit">Apply Filters</button>
       </form>
     </div>
   );
