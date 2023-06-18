d8ecf655bf96e504514c105075e44e735b9689ad
David Orlando Miranda Roa <domirandar@unal.edu.co>
2023-06-18 10:14:31 -0500

    fix(Client/Navbar)navbar persistent at scrolling, bblurred

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,33 @@
+import { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import NavMenu from '../NavMenu/NavMenu';
 import { AiOutlineShoppingCart } from 'react-icons/ai';
 import './Navbar.style.css';
 
 function Navbar() {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 0;
+      setScrolled(isScrolled);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <nav className="navbar">
+    <nav className={`navbar ${scrolled ? 'scrolled' : ''}`}>
       <NavMenu />
       <div className="navbar-title">Art & Culture</div>
       <div className="navlinks-container">
         <NavLink to="/" className="navlinks">
           Home
         </NavLink>
-
         <NavLink to="/create" className="navlinks">
           Create
         </NavLink>
