ce18964b81050983e13c1f74de330ab989a46c64
Ssamza <santiagoemezeta@gmail.com>
2023-06-29 22:21:29 -0500

    feat(actualizacion modo nocturno - Estilos view Users)

--- src/components/UserDetail/UserDetail.jsx
+++ src/components/UserDetail/UserDetail.jsx
@@ -1,4 +1,9 @@
-import { FaTwitter, FaFacebook, FaInstagram, FaPinterest} from 'react-icons/fa';
+import {
+  FaTwitter,
+  FaFacebook,
+  FaInstagram,
+  FaPinterest,
+} from 'react-icons/fa';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllUsers, getAllArts, getDetail } from '../../redux/actions';
@@ -9,28 +14,34 @@ import style from './UserDetail.module.css';
 const UserDetail = () => {
   const { userId } = useParams();
   const dispatch = useDispatch();
-  const allUsers = useSelector((state) => state.allUsers);//usuaro
-  const artworkId = useSelector((state)=> state.allArts);//obras
+  const allUsers = useSelector((state) => state.allUsers); //usuaro
+  const artworkId = useSelector((state) => state.allArts); //obras
 
-  const userDetail = allUsers.find((user) => user.userId === userId);//datos del usuario
+  const userDetail = allUsers.find((user) => user.userId === userId); //datos del usuario
   // const artworkIdArray = Array.isArray(artworkId) ? artworkId : [];
-  const filteredArtworks = artworkId.filter((artwork) => artwork.userId === userId);//si la obra coincide con el Id
-
+  const filteredArtworks = artworkId.filter(
+    (artwork) => artwork.userId === userId
+  ); //si la obra coincide con el Id
 
   useEffect(() => {
     dispatch(getAllUsers());
     dispatch(getAllArts());
-    dispatch(getDetail())
+    dispatch(getDetail());
   }, [dispatch, userId]);
 
-  const imagesDefault = ['https://www.bicaalu.com/wp-content/uploads/el_tarot_de_leonora_carrington.jpg',
-  'https://www.feelcats.com/wp-content/uploads/2014/11/Henrietta-Ronner_cuadro_011.jpg',
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPU0TPyuimzbBY8WaUq-5AUqAd4ONhQTUd7g&usqp=CAU',
-  'https://i0.wp.com/hotbook.mx/wp-content/uploads/2014/10/hotbook-94.jpg?fit=1024%2C768&ssl=1',
-  'https://i0.wp.com/arteyalgomas.com/wp-content/uploads/2020/01/Van_Gogh.-Campo-de-trigo-con-cuervos.-1890.jpg?resize=1140%2C713&ssl=1',
-  'https://i.pinimg.com/originals/fd/65/c5/fd65c5b4d77549893ee645706e30605c.jpg' ];
+  const imagesDefault = [
+    'https://www.bicaalu.com/wp-content/uploads/el_tarot_de_leonora_carrington.jpg',
+    'https://www.feelcats.com/wp-content/uploads/2014/11/Henrietta-Ronner_cuadro_011.jpg',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPU0TPyuimzbBY8WaUq-5AUqAd4ONhQTUd7g&usqp=CAU',
+    'https://i0.wp.com/hotbook.mx/wp-content/uploads/2014/10/hotbook-94.jpg?fit=1024%2C768&ssl=1',
+    'https://i0.wp.com/arteyalgomas.com/wp-content/uploads/2020/01/Van_Gogh.-Campo-de-trigo-con-cuervos.-1890.jpg?resize=1140%2C713&ssl=1',
+    'https://i.pinimg.com/originals/fd/65/c5/fd65c5b4d77549893ee645706e30605c.jpg',
+  ];
   //para elegir que se va ver en el carrusel
-  const images = filteredArtworks.length > 0 ? filteredArtworks.map((artwork) => artwork.image) : imagesDefault;
+  const images =
+    filteredArtworks.length > 0
+      ? filteredArtworks.map((artwork) => artwork.image)
+      : imagesDefault;
 
   //enlaces para las redes
   const handleTwitterShare = () => {
@@ -71,19 +82,35 @@ const UserDetail = () => {
         </ul>
         <div className={style['positionPhoto']}>
           {userDetail?.profilePicture && (
-            <img src={userDetail.profilePicture} alt="img" className={style['photoPerfil']} />
-            )}
+            <img
+              src={userDetail.profilePicture}
+              alt='img'
+              className={style['photoPerfil']}
+            />
+          )}
         </div>
         <div className={style['socialIcons']}>
-              <FaTwitter className={style['shareIcon']} onClick={handleTwitterShare} />
-              <FaFacebook className={style['shareIcon']} onClick={handleFacebookShare} />
-              <FaInstagram className={style['shareIcon']} onClick={handleInstagramShare} />
-              <FaPinterest className={style['shareIcon']}  onClick={handlePinterestShare} />
+          <FaTwitter
+            className={style['shareIcon']}
+            onClick={handleTwitterShare}
+          />
+          <FaFacebook
+            className={style['shareIcon']}
+            onClick={handleFacebookShare}
+          />
+          <FaInstagram
+            className={style['shareIcon']}
+            onClick={handleInstagramShare}
+          />
+          <FaPinterest
+            className={style['shareIcon']}
+            onClick={handlePinterestShare}
+          />
         </div>
       </div>
-        <div>
-          <CarruselUsers images={images} />
-        </div>
+      <div>
+        <CarruselUsers images={images} />
+      </div>
     </div>
   );
 };
--- src/views/Users/Users.jsx
+++ src/views/Users/Users.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate, useLocation } from 'react-router-dom';
-import { getAllUsers } from '../../redux/actions';
+import { getAllUsers, getAllArts } from '../../redux/actions';
 import UserCard from '../../components/UserCard/UserCard';
 import styles from './UsersPage.module.css';
 import UserPagination from '../../components/PgUser/PgUser';
@@ -12,14 +12,16 @@ const UsersPage = () => {
   const navigate = useNavigate();
   const locationUsers = useLocation();
   const allUsers = useSelector((state) => state.allUsers);
+  const allArts = useSelector((state) => state.allArts);
   const [currentUsersPage, setCurrentUsersPage] = useState(1);
-  const usersPerPage = 3;
+  const usersPerPage = 6;
   const indexOfLastUser = currentUsersPage * usersPerPage;
   const indexOfFirstUser = indexOfLastUser - usersPerPage;
   const currentUsers = allUsers.slice(indexOfFirstUser, indexOfLastUser);
 
   useEffect(() => {
     dispatch(getAllUsers());
+    dispatch(getAllArts());
   }, [dispatch]);
 
   useEffect(() => {
@@ -43,19 +45,24 @@ const UsersPage = () => {
 
   return (
     <div>
-      <NavLink className={styles['BttBack']} to='/'>
-        {' '}
-        ← BACK{' '}
-      </NavLink>
       <div className={styles.usersPage}>
         <div className={styles.userCards}>
           {currentUsers.map((user) => (
-            <UserCard key={user.userId} user={user} /* value={handleEditUser} */ />
+            <UserCard
+              key={user.userId}
+              user={user}
+              art={allArts} /* value={handleEditUser} */
+            />
           ))}
         </div>
       </div>
       <div className={styles['pagination']}>
-        <UserPagination usersPerPage={usersPerPage} totalUsers={allUsers.length} currentUsersPage={currentUsersPage} handlePageChange={handlePageChange} />
+        <UserPagination
+          usersPerPage={usersPerPage}
+          totalUsers={allUsers.length}
+          currentUsersPage={currentUsersPage}
+          handlePageChange={handlePageChange}
+        />
       </div>
     </div>
   );
