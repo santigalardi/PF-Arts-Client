dce62d9fc223d7560e4a5d17c43f633fc64fc4d4
David Orlando Miranda Roa <domirandar@unal.edu.co>
2023-07-04 13:52:47 -0500

    railway redeploy

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const URL = 'https://pf-arts-api-production.up.railway.app/';
+const URL = 'https://pf-arts-api-production.up.railway.app';
 //const URL = 'http://localhost:3001';
 
 // -------- ARTS ---------
