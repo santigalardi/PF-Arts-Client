aa6fad69ef59a63158191fada78f90ea3d3a9fb1
David Orlando Miranda Roa <domirandar@unal.edu.co>
2023-07-14 10:11:46 -0500

    Restore(main)commented line local dev

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
+import axios from 'axios';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import axios from 'axios'; // Importamos axios
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './redux/store';
 import App from './App';
@@ -10,6 +10,7 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './index.css';
 
 axios.defaults.baseURL = 'https://pf-arts-api-production.up.railway.app';
+//axios.defaults.baseURL = 'https://localhost:3001';
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
